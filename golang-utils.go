cff689b4ff0e5ec4537ee70a1bab2e9b78ace3cd
KuangMing_Chen <kuangming_chen@htc.com>
2014-05-08 11:19:11 +0800

    Add http useful functions

--- golang-utils.go
+++ golang-utils.go
@@ -3,7 +3,15 @@ package goutils
 import (
 	"bufio"
 	"bytes"
+	"code.google.com/p/go-uuid/uuid"
+	"crypto/sha256"
 	"encoding/binary"
+	"encoding/json"
+	"fmt"
+	"github.com/spaolacci/murmur3"
+	"io/ioutil"
+	"log"
+	"net/http"
 	"os"
 )
 
@@ -25,7 +33,7 @@ func ByteToUint64(data []byte) uint64 {
 // RandomUint64 creates a random uint64 number
 func RandomUint64(data string) uint64 {
 	hasher := murmur3.New64()
-	hasher.Write([]byte(NewUUID()))
+	hasher.Write([]byte(uuid.NewUUID()))
 	return hasher.Sum64()
 }
 
@@ -57,4 +65,87 @@ func WriteByteToFile(filename string, data [][]byte) error {
 		}
 	}
 	return nil
+}
+
+type p struct {
+	a, b int
+}
+
+// SessionSignature generates a hashed sigature
+// It also converts from struct to []byte
+func SessionSignature(data *p) ([]byte, error) {
+	h := sha256.New()
+	b, err := json.Marshal(data)
+	if err != nil {
+		return nil, nil
+	}
+	_, err = h.Write(b)
+	if err != nil {
+		return nil, nil
+	}
+	return h.Sum([]byte{}), nil
+}
+
+// Make a http GET function
+func HttpGet() {
+	res, err := http.Get("http://www.google.com")
+	if err != nil {
+		log.Fatal(err)
+	}
+	robots, err := ioutil.ReadAll(res.Body)
+	res.Body.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%s", robots)
+}
+
+// HTTP handler function
+func handler(w http.ResponseWriter, r *http.Request) {
+	resp, err := http.DefaultClient.Do(r)
+	defer resp.Body.Close()
+	if err != nil {
+		panic(err)
+	}
+	for k, v := range resp.Header {
+		for _, vv := range v {
+			w.Header().Add(k, vv)
+		}
+	}
+	for _, c := range resp.SetCookie {
+		w.Header().Add("Set-Cookie", c.Raw)
+	}
+	w.WriteHeader(resp.StatusCode)
+	result, err := ioutil.ReadAll(resp.Body)
+	if err != nil && err != os.EOF {
+		panic(err)
+	}
+	w.Write(result)
+}
+
+// Define route and write back response
+func main() {
+	http.HandleFunc("/", handler)
+	log.Println("Start serving on port 8888")
+	http.ListenAndServe(":8888", nil)
+	os.Exit(0)
+}
+
+// Find any available address
+func listenTCP() (net.Listener, string) {
+	l, e := net.Listen("tcp", "127.0.0.1:0") // any available address
+	if e != nil {
+		log.Fatalf("net.Listen tcp :0: %v", e)
+	}
+	return l, l.Addr().String()
+}
+
+// genCode generates n digits of access code
+func genCode(n uint32) string {
+	rand.Seed(time.Now().Unix())
+	var str string
+	for i := uint32(0); i < n; i++ {
+		str = fmt.Sprintf("%s%s", str, strconv.Itoa(rand.Intn(10)))
+	}
+	return str
 }
